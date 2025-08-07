199237a13f9f354e66462fd08d16636975b6bc04
ybunhong <ybunhong369@gmail.com>
2025-08-07 14:51:59 +0700

    feat: add assets and modify root

--- src/component/main.js
+++ src/component/main.js
@@ -3,3 +3,5 @@ import "../style/style.css";
 import "../component/base-button/base-button";
 import "../component/header/header";
 import "../component/hero-section/hero-section";
+import "../component/main-footer/main-footer";
+import "../component/icon-button/icon-button";
